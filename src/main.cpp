f778d1ddc7349e0ce3be6fcfb59f672dce044831
MitchellHansen <mitchellhansen4@gmail.com>
2017-01-13 22:36:44 -0800

    Fixed up the way the vertex array is passed, added scale text and x axis lines

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,37 @@ const int WINDOW_SIZE_Y = 800;
 
 int data_point_count(std::string);
 std::vector<std::vector<float>> read_data(std::string);
-void draw_line_graph(std::vector<std::vector<float>>, sf::RenderWindow *window);
+std::vector<sf::VertexArray> draw_line_graph(std::vector<std::vector<float>>);
+sf::Vector2f get_largest_smallest_data_points(std::vector<std::vector<float>> data);
+void draw_graph_background(int segment_count, sf::RenderWindow *window);
 
 int main() {
 
 	sf::RenderWindow window(sf::VideoMode(WINDOW_SIZE_X, WINDOW_SIZE_Y), "cs445");
 
+
+	// This is where the data is read, change the path for another file
 	std::vector<std::vector<float>> data = read_data("../data/car_mpg.txt");
+
+	sf::Font f;
+	f.loadFromFile("../assets/code_bold.otf");
+
+	sf::Text y_max_text;
+	y_max_text.setFont(f);
+	y_max_text.setString(std::to_string((int)get_largest_smallest_data_points(data).y));
+	y_max_text.setCharacterSize(14);
+	y_max_text.setColor(sf::Color::Black);
+	y_max_text.setPosition(sf::Vector2f(10, 10));
+
+	sf::Text x_max_text;
+	x_max_text.setFont(f);
+	x_max_text.setString(std::to_string(data.at(0).size()));
+	x_max_text.setCharacterSize(14);
+	x_max_text.setColor(sf::Color::Black);
+	x_max_text.setPosition(sf::Vector2f(WINDOW_SIZE_X - 30, WINDOW_SIZE_Y - 30));
+
+
+	std::vector<sf::VertexArray> lines = draw_line_graph(data);
 
 	while (window.isOpen()) {
 
@@ -31,9 +55,13 @@ int main() {
 
 		window.clear(sf::Color(255, 255, 255));
 		
-		draw_line_graph(data, &window);
+		draw_graph_background(data.at(0).size(), &window);
 
+		for (auto i : lines)
+			window.draw(i);
 
+		window.draw(y_max_text);
+		window.draw(x_max_text);
 		window.display();
 
 	}
@@ -49,7 +77,6 @@ sf::Vector2f get_largest_smallest_data_points(std::vector<std::vector<float>> da
 	for (int x = 0; x < data.at(0).size(); x++) {
 		for (int y = 0; y < data.size(); y++) {
 
-
 			if (data.at(y).at(x) < smallest)
 				smallest = data.at(y).at(x);
 
@@ -61,39 +88,74 @@ sf::Vector2f get_largest_smallest_data_points(std::vector<std::vector<float>> da
 	return sf::Vector2f(smallest, largest);
 }
 
-void draw_line_graph(std::vector<std::vector<float>> d, sf::RenderWindow *window) {
+
+void draw_graph_background(int segment_count, sf::RenderWindow *window) {
 	
+	// The x increment that we step by
+	int step = WINDOW_SIZE_X / segment_count;
+
+	int pos = 0;
+
+	while (pos < WINDOW_SIZE_X) {
+		
+		sf::RectangleShape line(sf::Vector2f(2, WINDOW_SIZE_Y));
+		line.setPosition(sf::Vector2f(pos, 0));
+		line.setFillColor(sf::Color::Black);
+		window->draw(line);
+
+		pos += step;
+	}
+	
+
+
+
+}
+
+// This function takes the 2d array of data and returns multiple vertex arrays to draw
+std::vector<sf::VertexArray> draw_line_graph(std::vector<std::vector<float>> d) {
+	
+	// Get the datas highest and lowest values to scale with
 	sf::Vector2f bounds = get_largest_smallest_data_points(d);
 
-	sf::VertexArray v;
-
-	v.setPrimitiveType(sf::LinesStrip);
-	
-
+	// The x increment that we step by
 	int step = WINDOW_SIZE_X / d.at(0).size();
+
+	std::vector<sf::VertexArray> vertex_array_array;
 
 	for (int x = 0; x < d.size(); x++) {
 
 		int x_pos = 0;
-		//sf::Color c(rand()%255, rand()%255, rand()%255);
+
+		sf::VertexArray v;
+		v.setPrimitiveType(sf::LinesStrip);
+		
+		// Set the line to a random color		
+		sf::Color color(rand() % 255, rand() % 255, rand() % 255);
 
 		for (int y = 0; y < d.at(x).size(); y++) {
 
 			sf::Vertex v1(sf::Vector2f(x_pos, WINDOW_SIZE_Y - d.at(x).at(y) * (WINDOW_SIZE_Y / bounds.y )));
-			//sf::Vertex v2(sf::Vector2f(x_pos, WINDOW_SIZE_Y - d.at(y).at(x) * (WINDOW_SIZE_Y / bounds.y )));
-			v1.color = sf::Color::Blue;
-			//v2.color = c;
+			v1.color = color;
+
 			v.append(v1);
 
 			x_pos += step;
 		}
 
-		window->draw(v);
-		v.clear();
+		vertex_array_array.push_back(v);
 	}
+
+	return vertex_array_array;
 
 }
 
+
+
+// This function reads in a file line by line
+// Each line is tokenized by spaces, commas, or by quoted strings
+// The tokens are then converted to floats and added to an array
+// Once a line has been read and put into the vector, that vector is then added to another vector for storage
+// When done, the 2d vector is then returned
 
 std::vector<std::vector<float>> read_data(std::string file_path) {
 	
